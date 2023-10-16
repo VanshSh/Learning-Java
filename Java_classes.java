26dac1317e79f9e080eb0c5a8e76ce2f0f19f2ff
VanshSh <vanshsharma9354@gmail.com>
2023-10-16 22:48:36 +0530

    feat: :sparkles: Java Class attributes

--- Java_classes.java
+++ Java_classes.java
@@ -1,11 +1,17 @@
 public class Java_classes {
     int x = 5;
+    String name = "Vansh Sharma";
+    String fname = "John";
+    String lname = "Doe";
+     // final String name = "Vansh Sharma";
 
     public static void main(String[] args) {
         Java_classes obj1 = new Java_classes();
         Java_classes obj2 = new Java_classes();
-        System.out.println(" U_U L-7 in java_classes.java => " + obj1.x);
-        System.out.println(" U_U L-7 in java_classes.java => " + obj2.x);
+        obj1.name = "Sharma";
+        System.out.println(" U_U L-7 in java_classes.java => " + obj1.name);
+        System.out.println(" U_U L-7 in java_classes.java => " + obj2.name);
+        System.out.println(" U_U L-14 in java_classes.java => " + obj1.fname + " " + obj2.lname);
         
 
         
