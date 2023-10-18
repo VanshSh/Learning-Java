1be5397daf3fb31c21e38d511beeb7f43bedec8b
VanshSh <vanshsharma9354@gmail.com>
2023-10-18 23:00:04 +0530

    feat: :sparkles: Creating the custom package

---
+++ mypack/MyPackageClass.java
@@ -0,0 +1,13 @@
+package mypack;
+
+class MyPackageClass {
+    public static void main(String[] args) {
+        System.out.println("This is my package!");
+    }
+}
+
+// STep 1 MyPackageClass.java should be in the same package named folder "mypack"
+// Step 2 C:\Users\Your Name>javac MyPackageClass.java
+// Step 3 C:\Users\Your Name>javac -d . MyPackageClass.java
+// C:\Users\Your Name>java mypack.MyPackageClass
+
