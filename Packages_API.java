ea276a2cbe8b0dfd26a82e2956520f1efd8e3457
VanshSh <vanshsharma9354@gmail.com>
2023-10-18 22:38:10 +0530

    feat: :sparkles: Packages/API
    Import classes

---
+++ Packages_API.java
@@ -0,0 +1,12 @@
+
+// Importing class
+import java.util.Scanner;
+
+public class Packages_API {
+    public static void main(String[] args) {
+        Scanner userLine = new Scanner(System.in);
+        System.out.println(" Enter your name");
+        String userName = userLine.nextLine();
+        System.out.println(" Your name is: " + userName);
+    }
+}
