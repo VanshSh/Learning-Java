72c80cfde9ebb7441d3113edf8742d95d6fa1288
VanshSh <vanshsharma9354@gmail.com>
2023-10-25 22:19:15 +0530

    feat: :sparkles: Local Time

--- DateTime.java
+++ DateTime.java
@@ -1,8 +1,13 @@
 import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.LocalDateTime;
 
 public class DateTime {
     public static void main(String[] args) {
         LocalDate date = LocalDate.now();
         System.out.println(" U_U L-6 in DateTime.java => " + date);
+
+        LocalTime time = LocalTime.now();
+        System.out.println(" U_U L-10 in DateTime.java => " + time);
     }
 }
