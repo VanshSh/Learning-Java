3f7619bd28be5323128f63d53d1b9efd5bdb2e3b
VanshSh <vanshsharma9354@gmail.com>
2023-10-25 21:48:19 +0530

    feat: :sparkles: Enums

---
+++ Enums.java
@@ -0,0 +1,23 @@
+enum Level {
+    LOW,
+    HIGH,
+    MEDIUM
+}
+
+public class Enums {
+    public static void main(String[] args) {
+        Level myVar = Level.MEDIUM;
+        switch (myVar) {
+            case LOW:
+                System.out.println("Low level");
+                break;
+            case MEDIUM:
+                System.out.println("Medium level");
+                break;
+            case HIGH:
+                System.out.println("High level");
+                break;
+        }
+
+    }
+}
