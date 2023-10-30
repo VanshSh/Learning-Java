cdae455a02c8a8124bcb07f237d0155eee2b8984
VanshSh <vanshsharma9354@gmail.com>
2023-10-30 22:10:06 +0530

    feat: :sparkles: Implementing method for Java Thread

--- ThreadsInJava.java
+++ ThreadsInJava.java
@@ -1,11 +1,26 @@
 // Extending method for thread
-public class ThreadsInJava extends Thread {
-  public static void main(String[] args) {
-    ThreadsInJava thread = new ThreadsInJava();
-    thread.start();
-    System.out.println("This code is outside of the thread");
-  }
-  public void run() {
-    System.out.println("This code is running in a thread");
-  }
+
+// public class ThreadsInJava extends Thread {
+//   public static void main(String[] args) {
+//     ThreadsInJava thread = new ThreadsInJava();
+//     thread.start();
+//     System.out.println("This code is outside of the thread");
+//   }
+//   public void run() {
+//     System.out.println("This code is running in a thread");
+//   }
+// }
+
+// Implementing methods for the thread
+public class ThreadsInJava implements Runnable {
+    public static void main(String[] args) {
+        ThreadsInJava obj = new ThreadsInJava();
+        Thread thread = new Thread(obj);
+        thread.start();
+        System.out.println("This code is outside of the thread");
+    }
+
+    public void run() {
+        System.out.println("This code is running in a thread");
+    }
 }
