1126457517fc03250d8c8120312d205cbe6aa43f
VanshSh <vanshsharma9354@gmail.com>
2023-11-03 22:57:25 +0530

    feat:

---
+++ questionInJava.java
@@ -0,0 +1,20 @@
+// SUM OF NUMBERS 👇🏻
+// import java.util.Scanner;
+
+// public class questionInJava {
+//     public static void main(String[] args) {
+//         int x, y, sum;
+
+//         Scanner sumOfNumbers = new Scanner(System.in);
+//         System.out.println("Type a number:");
+//         x = sumOfNumbers.nextInt(); // Read user input
+
+//         System.out.println("Type another number:");
+//         y = sumOfNumbers.nextInt(); // Read user input
+
+//         sum = x + y; // Calculate the sum of x + y
+//         System.out.println("Sum is: " + sum); // Print the sum
+
+//     }
+
+// }
